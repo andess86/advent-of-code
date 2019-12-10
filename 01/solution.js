8059af900e1f183d2f1bbc0fae140b89e6f511f8
Anders Svensson <andess86@gmail.com>
2019-12-10 16:13:18 +0100

    Solved day 1 using recursion

--- 01/solution.js
+++ 01/solution.js
@@ -7,11 +7,24 @@ const calculateFuel = module => {
   return Math.floor(module / 3) - 2;
 };
 
+const calculateTotalFuel = modules => {
+  if (modules.length === 1) {
+    return calculateFuel(modules[0]);
+  }
+  return (
+    calculateFuel(modules[0]) +
+    calculateTotalFuel(modules.slice(1, modules.length))
+  );
+};
+
 //Day 1, first half
 a.then(mass => {
   return mass.reduce((acc, module) => {
     return acc + calculateFuel(module);
   }, 0);
 }).then(res => {
-  console.log('First half, ', res);
+  console.log("First half, ", res);
 });
+
+//Day 1, first half, using recursion
+a.then(modules => calculateTotalFuel(modules)).then(data => console.log("First half, ", data));
