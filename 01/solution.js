a4a826faed907dab4b185c0ed7d24c5b8b1bbd50
Anders Svensson <andess86@gmail.com>
2019-12-10 16:10:05 +0100

    Clean up solution

--- 01/solution.js
+++ 01/solution.js
@@ -3,9 +3,15 @@ const { getRows } = require("./utils");
 let a = getRows();
 let sum = 0;
 
+const calculateFuel = module => {
+  return Math.floor(module / 3) - 2;
+};
+
+//Day 1, first half
 a.then(mass => {
-  mass.forEach(element => {
-    sum += Math.floor(element / 3) -2;
-  });
-  console.log(sum);
+  return mass.reduce((acc, module) => {
+    return acc + calculateFuel(module);
+  }, 0);
+}).then(res => {
+  console.log('First half, ', res);
 });
