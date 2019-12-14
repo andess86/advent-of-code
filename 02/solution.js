04d74518d91d9e752055655b5403a0692163765a
Anders Svensson <andess86@gmail.com>
2019-12-14 22:22:09 +0100

    Clean up day 2 solution

--- 02/solution.js
+++ 02/solution.js
@@ -1,72 +1,56 @@
 const { getRows } = require("../utils");
+const input = getRows();
 
+//Some kind of mapping object for readabilty
 const opCode = {
   add: 1,
   multiply: 2,
   abort: 99
 };
 
-let programSequence = getRows()
-  .then(data => data.toString())
-  .then(data => data.split(","))
-  .then(data => {
-    data = data.map(Number);
-    data[1] = 12;
-    data[2] = 2;
-    for (let index = 0; index < data.length - 4; index += 1) {
-      let val1 = data[index + 1];
-      let val2 = data[index + 2];
-      let dest = data[index + 3];
+const programSequence = input =>
+  input
+    .then(data => data.toString()) //To be able to use split
+    .then(data => data.split(",")) //Split on ","
+    .then(data => {
+      data = data.map(Number); //Convert array content to int to be able to calculate
 
-      switch (data[index]) {
-        case opCode.add:
-          console.log("--------------------------");
-          console.log("index: ", index);
-          console.log("--- Adding ---");
-          console.log("destination", dest);
-          data[dest] = data[val1] + data[val2];
+      //To do this, before running the program,
+      //replace position 1 with the value 12 and
+      // replace position 2 with the value 2.
+      data[1] = 12;
+      data[2] = 2;
 
-          console.log(data[dest]);
+      //Loop over programSequence
+      for (let index = 0; index < data.length - 4; index += 1) {
+        let val1 = data[index + 1];
+        let val2 = data[index + 2];
+        let dest = data[index + 3];
 
-          index += 3;
-          break;
+        switch (data[index]) {
+          case opCode.add:
+            data[dest] = data[val1] + data[val2];
+            index += 3;
+            break;
 
-        case opCode.multiply:
-          console.log("--------------------------");
-          console.log("index: ", index);
-          console.log("--- Multiplying ---");
-          console.log(data[dest]);
+          case opCode.multiply:
+            data[dest] = data[val1] * data[val2];
+            index += 3;
+            break;
 
-          data[dest] = data[val1] * data[val2];
+          case opCode.abort:
+            index += 0;
+            break;
 
-          console.log(data[dest]);
-
-          index += 3;
-          break;
-
-        case opCode.abort:
-          console.log("--------------------------");
-          console.log("index: ", index);
-          console.log("--- 99, break ---");
-          index += 0;
-          // console.log(data[index]);
-          break;
-
-        default:
-          break;
+          default:
+            break;
+        }
       }
-    }
-    console.log("Right answer:", data[0]);
-  });
+      console.log(
+        "The right answer is:",
+        data[0],
+        "is the number at position 0 in the program sequence."
+      );
+    });
 
-const add = (programSequence, value1, value2, destination) => {};
-const multiply = (programSequence, value1, value2, destination) => {};
-const abort = () => {};
-
-//Think I will need some kind of mapping object between values in program sequence and function
-//to perform
-// const operation = {
-//   1: add(),
-//   2: multiply(),
-//   99: abort()
-// };
+programSequence(input);
